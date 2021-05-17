82a0727d29d715f59cc86aaea1d18847e2439de0
onion6677 <84284632+onion6677@users.noreply.github.com>
2021-05-17 17:37:50 +0900

    Delete JS.js

--- JS.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-{
-    const imgs = [].slice.call(document.querySelectorAll("#main-wrapper>a>img"));
-    document.querySelector(".sub-wrapper").addEventListener("click", e => {
-        if(e.target.type != "image"){return;}
-        imgs.forEach(img => img.className = e.target.name == img.id ? "on" : "off");
-    });
-}
